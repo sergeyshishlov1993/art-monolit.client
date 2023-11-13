e988d90d9fa47d8b8f0eec665703ecb6d924a6df
sergey shishlov <shishlovsergey040593@gmail.com>
2023-11-13 19:32:06 +0200

    update test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,59 @@ export default defineNuxtConfig({
   css: ["~/assets/css/app.scss"],
   ssr: false,
   modules: ["@pinia/nuxt"],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: "mask-icon",
+          // href: "/safari-pinned-tab.svg",
+          href: "/safari-pinned-tab.s",
+          color: "#5bbad5",
+        },
+
+        {
+          rel: "apple-touch-icon",
+          type: "image/png",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+
+        {
+          rel: "mask-icon",
+          href: "/safari-pinned-tab.svg",
+          color: "#5bbad5",
+        },
+      ],
+
+      meta: [
+        {
+          name: "msapplication-TileColor",
+          content: "#da532c",
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff",
+        },
+      ],
+      title: "АРТ - МОНОЛІТ",
+    },
+  },
 });
