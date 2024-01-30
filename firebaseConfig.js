1e904cab761c33f3271c608a8362df638b3f941f
sergey shishlov <shishlovsergey040593@gmail.com>
2024-01-30 12:37:58 +0200

    fix firebase bag

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -12,23 +12,23 @@ import { getApps, initializeApp, getApp } from "firebase/app";
 
 // -------------config-------------
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDbG9D8Liuyqtn8bmNaYDuNh9piv6Eoq94",
-//   authDomain: "art-monolit-8898c.firebaseapp.com",
-//   projectId: "art-monolit-8898c",
-//   storageBucket: "art-monolit-8898c.appspot.com",
-//   messagingSenderId: "382177457226",
-//   appId: "1:382177457226:web:9641737a3911d18bbc22b3",
-// };
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_AUTH_DOMAIN,
-  storageBucket: import.meta.env.VITE_PROJECT_ID,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyDbG9D8Liuyqtn8bmNaYDuNh9piv6Eoq94",
+  authDomain: "art-monolit-8898c.firebaseapp.com",
+  projectId: "art-monolit-8898c",
+  storageBucket: "art-monolit-8898c.appspot.com",
+  messagingSenderId: "382177457226",
+  appId: "1:382177457226:web:9641737a3911d18bbc22b3",
 };
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_AUTH_DOMAIN,
+//   storageBucket: import.meta.env.VITE_PROJECT_ID,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID,
+// };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
