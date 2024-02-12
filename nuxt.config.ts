76e67294ceaf4e7e983a8223d1e62f08b66d1157
sergey shishlov <shishlovsergey040593@gmail.com>
2024-02-12 13:02:56 +0200

    test metatag 11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -135,7 +135,8 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      google_analytics_id: "AW-16457131804", // Убедитесь, что здесь указан правильный идентификатор Google Analytics
+      google_analytics_id: "G-53XP3ZSTBN", // Убедитесь, что здесь указан правильный идентификатор Google Analytics
+      // google_analytics_id: "AW-16457131804",
       production_mode: Boolean(isProduction), // Предполагая, что isProduction - это переменная, определяющая режим продакшн
     },
   },
