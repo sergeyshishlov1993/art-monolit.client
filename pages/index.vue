c0dad546ee3d7db9715d9721fec30a44fcc3b9bd
sergey shishlov <shishlovsergey040593@gmail.com>
2024-09-16 10:11:58 +0300

    change pixel facebook

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,15 @@
       <Meta property="og:type" content="website" />
     </Head>
 
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style="display: none"
+        src="https://www.facebook.com/tr?id=2021575691610536&ev=PageView&noscript=1"
+      />
+    </noscript>
+
     <section-main-scrin />
     <section-catalog-preview />
     <section-our-works />
@@ -88,6 +97,22 @@ useHead({
              function gtag(){dataLayer.push(arguments);}
              gtag('js', new Date());
              gtag('config', 'AW-16457131804');`,
+    },
+
+    // Meta Pixel Code
+    {
+      innerHTML: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '2021575691610536');
+      fbq('track', 'PageView');
+      `,
     },
   ],
 });
