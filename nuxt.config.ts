606bbf1b22d49618e30ef290cebd2cebdc3c19d1
sergey shishlov <shishlovsergey040593@gmail.com>
2025-08-06 15:18:02 +0300

    test gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,6 +48,15 @@ export default defineNuxtConfig({
       ],
 
       script: [
+          {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5NLD2FPG');`,
+
+          tagPosition: 'head',
+        },
         {
           children: `
             !function(f,b,e,v,n,t,s)
@@ -63,6 +72,14 @@ export default defineNuxtConfig({
           `,
           type: "text/javascript",
         },
+      ],
+
+      noscript: [
+        {
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`, // <-- ЗАМЕНИТЕ НА ВАШ ID
+          tagPosition: 'bodyOpen',
+        }
       ],
     },
   },
